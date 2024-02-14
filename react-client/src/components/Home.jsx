87190fd9c579c96543a68af0d407d650394f6e55
jescares <jescares@my.centennialcollege.ca>
2024-02-14 10:08:31 -0500

    initialized react client

---
+++ react-client/src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React, { Component }  from 'react';
+// HOME PAGE
+function Home(props)
+{
+
+
+    return (
+        <div>
+            <h2>Lab Assignment #1 - Express Server with React Client</h2>
+            <h4>Name: Jan Rodel Escareses
+                <br/>Student ID: 301225122
+            </h4>
+        </div>
+    );
+
+}
+export default Home;
